c3127ccf770ef250f97e36718f76686489414d66
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-02-10 14:00:57 -0300

    feat: update AnimeModule to include additional services and repository provider

--- apps/api/src/anime/anime.module.ts
+++ apps/api/src/anime/anime.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AnimeResolver } from './anime.resolver';
+import { AnimeService } from './anime.service';
+import { SeasonService } from 'src/season/season.service';
+import { StudioService } from 'src/studio/studio.service';
+import { AnimeRepository } from './repositories/anime.repository';
+import { InMemoryAnimeRepository } from './repositories/in-memory-anime.repository';
 
 @Module({
-  providers: [AnimeResolver],
+  providers: [
+    AnimeResolver,
+    AnimeService,
+    SeasonService,
+    StudioService,
+    {
+      provide: AnimeRepository,
+      useClass: InMemoryAnimeRepository,
+    },
+  ],
 })
 export class AnimeModule {}
