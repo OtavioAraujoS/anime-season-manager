52b4c4ebd7e363b0aa15539fdf1c990cf09512e0
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-02-10 09:47:19 -0300

    feat: define Season model with fields and register SeasonPeriod enum

--- apps/api/anime/models/season.model.ts
+++ apps/api/anime/models/season.model.ts
@@ -1,6 +1,24 @@
+import { ObjectType, Field, ID, Int, registerEnumType } from '@nestjs/graphql';
+
 export enum SeasonPeriod {
   WINTER = 'WINTER',
   SPRING = 'SPRING',
   SUMMER = 'SUMMER',
   FALL = 'FALL',
+}
+
+registerEnumType(SeasonPeriod, {
+  name: 'SeasonPeriod',
+});
+
+@ObjectType()
+export class Season {
+  @Field(() => ID)
+  id!: string;
+
+  @Field(() => Int)
+  year!: number;
+
+  @Field(() => SeasonPeriod)
+  period!: SeasonPeriod;
 }
